754bd4323b56976a1ac682c133a1494f8e1a94b4
Basia <b.kumor@yahoo.com>
2020-11-29 18:01:00 +0100

    styling

--- index.php
+++ index.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP todo list</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 <body>
-    
+    <main class="main">
+        <section class="section-add">
+            <form action="">
+                <input type="text" name="title" placeholder="todo title" class="input-add"/>
+                <button type="submit" name="submit" class="btn-add">Add &nbsp; <span class="plus-sign">&#43;</span></button>
+            </form>
+        </section>
+        <section class="section-list">
+            <div class="todo-item">
+                <input type="checkbox" class="todo-checkbox">
+                <h2 class="todo-title">Title</h2>
+                <small class="todo-date">created: 29.11.2020</small>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
